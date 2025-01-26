7c042a1c5bfd909b9fc7acb16da9b5f0055e46cc
Madhavi8074 <madhavi738685@gmail.com>
2025-01-26 19:49:02 +0530

    Add files via upload

---
+++ Patterns3.java
@@ -0,0 +1,26 @@
+public class Patterns4 {
+    public static void main(String[] args) {
+        System.out.println("forth Pattern: ");
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    
+    }
+
+}
